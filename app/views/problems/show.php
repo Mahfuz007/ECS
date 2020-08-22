2e0b29c9d870b21659011a649a1c2848c93fc29b
Mahfuz <aamahfuz2@gmail.com>
2020-08-22 12:39:57 +0600

    added update button in problem show page

--- app/views/problems/show.php
+++ app/views/problems/show.php
@@ -6,7 +6,16 @@
         <div class="col-9">
             <div class="container ui segment">
                 <h2 class="center"><?php echo $data->name;?></h2>
-                <p>Problem Id: <?php echo $data->id;?></p>
+                <div class="row">
+                    <div class="col-10">
+                        <p>Problem Id: <?php echo $data->id;?></p>
+                    </div>
+                    <div class="col-2">
+                        <?php if($data->userid == $_SESSION['id']){ ?>
+                            <a class="btn btn-primary" href="<?php echo URLROOT;?>problems/update/<?php echo $data->id;?>">Update</a>
+                        <?php } ?>
+                    </div>
+                </div>
                 <p>Author: <a href="<?php echo URLROOT;?>users/profile/<?php echo $data->userid;?>"><?php echo $data->author ;?></a></p>
                 <p><?php echo $data->description;?></p>
                 <a class="ui primary button center" href="<?php echo URLROOT;?>problems/submit/<?php echo $data->id;?>">Submit</a>
