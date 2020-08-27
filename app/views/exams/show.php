1db2f9ef09666dd140bd9e4fafc5cb9fe4a1f1df
Mahfuz <aamahfuz2@gmail.com>
2020-08-27 14:06:51 +0600

    added timer for exams

--- app/views/exams/show.php
+++ app/views/exams/show.php
@@ -4,58 +4,77 @@
     <div class="row">
         <!-- Main Part -->
         <div class="col-9">
-            <div class="container ui segment">
 
-                <h1 class="center">Exam Information</h1>
-
-                <!-- Exam details -->
-                <div class="row">
-                    <div class="col-10">
-                        <p><strong>Type: </strong><?php echo $data[sizeof($data) - 1]->type; ?></p>
+            <?php if ($_SESSION['id']!= $data[sizeof($data) - 1]->author && date("Y-m-d H:i:s") < $data[sizeof($data) - 1]->begin_time) { ?>
+                <div class="container ui segment">
+                    <div class="timer">
+                        <span class="" id="hours">00</span>
+                        :
+                        <span class="" id="minutes">00</span>
+                        :
+                        <span class="" id="seconds">00</span>
                     </div>
-                    <div class="col-2">
-
-                        <?php if($data[sizeof($data) - 1]->author==$_SESSION['id']){ ?>
-                            <a class="text-secondary" target="_blank" href="<?php URLROOT;?>exams/standing/<?php echo $data[sizeof($data) - 1]->id;?>"><strong>Standing</strong></a>
-                        <?php } ?>
-
-                    </div>
+                    <input type="hidden" name="_time" value="<?php echo strtotime($data[sizeof($data) - 1]->begin_time);?>">
                 </div>
+            <?php } else { ?>
 
-                <p><strong>Title: </strong><?php echo $data[sizeof($data) - 1]->title; ?></p>
+                <div class="container ui segment">
 
-                <p><strong>Date and Time: </strong><?php echo $data[sizeof($data) - 1]->begin_time; ?> <strong><span class="text-success"><?php echo (date("Y-m-d H:is")> $data[sizeof($data) - 1]->end)?"(Ended)":"";?></span></strong> 
+                    <h1 class="center">Exam Information</h1>
 
-                <strong><span class="text-danger"><?php echo (date("Y-m-d H:is")<=$data[sizeof($data) - 1]->end && date("Y-m-d H:is")>=$data[sizeof($data) - 1]->begin_time)?"(Running)":"";?></span></strong> 
-            
-                </p>
+                    <!-- Exam details -->
+                    <div class="row">
+                        <div class="col-10">
+                            <p><strong>Type: </strong><?php echo $data[sizeof($data) - 1]->type; ?></p>
+                        </div>
+                        <div class="col-2">
 
+                            <?php if ($data[sizeof($data) - 1]->author == $_SESSION['id']) { ?>
+                                <a class="text-secondary" target="_blank" href="<?php URLROOT; ?>exams/standing/<?php echo $data[sizeof($data) - 1]->id; ?>"><strong>Standing</strong></a>
+                            <?php } ?>
 
-                <p><strong>Duration: </strong><?php echo $data[sizeof($data) - 1]->duration; ?></p>
-                <p><strong>Author: <a target="_blank" href="<?php echo URLROOT; ?>/users/profile/<?php echo $data[sizeof($data) - 1]->author; ?>"><?php echo $data[sizeof($data) - 1]->authorName; ?></a></strong></p>
-                
-
-                <!-- Display Problem Set -->
-                <h2 class="center">Problem Set</h2>
-
-                <?php $x ='A';?>
-
-                <?php for ($i = 0; $i < sizeof($data) - 1; $i++) { ?>
-                    <div class="ui celled list">
-                        <div class="item">
-                            <div class="shadow p-3">
-
-                                <a href="<?php echo URLROOT; ?>problems/delete/<?php echo $data[$i]->id; ?>"><i class="trash alternate icon text-danger"></i></a>
-
-                                <a target="_blank" href="<?php echo URLROOT; ?>problems/show/<?php echo $data[$i]->id; ?>"><?php echo $x++;?></a>
-                                -
-                                <a target="_blank" href="<?php echo URLROOT; ?>problems/show/<?php echo $data[$i]->id; ?>"><?php echo $data[$i]->name; ?></a>
-                            </div>
                         </div>
                     </div>
-                <?php } ?>
-            </div>
+
+                    <p><strong>Title: </strong><?php echo $data[sizeof($data) - 1]->title; ?></p>
+
+                    <p><strong>Date and Time: </strong><?php echo $data[sizeof($data) - 1]->begin_time; ?> <strong><span class="text-success"><?php echo (date("Y-m-d H:is") > $data[sizeof($data) - 1]->end) ? "(Ended)" : ""; ?></span></strong>
+
+                        <strong><span class="text-danger"><?php echo (date("Y-m-d H:is") <= $data[sizeof($data) - 1]->end && date("Y-m-d H:is") >= $data[sizeof($data) - 1]->begin_time) ? "(Running)" : ""; ?></span></strong>
+
+                    </p>
+
+
+                    <p><strong>Duration: </strong><?php echo $data[sizeof($data) - 1]->duration; ?></p>
+                    <p><strong>Author: <a target="_blank" href="<?php echo URLROOT; ?>/users/profile/<?php echo $data[sizeof($data) - 1]->author; ?>"><?php echo $data[sizeof($data) - 1]->authorName; ?></a></strong></p>
+
+                    <input type="hidden" name="_time" value="<?php echo null?>">
+
+
+                    <!-- Display Problem Set -->
+                    <h2 class="center">Problem Set</h2>
+
+                    <?php $x = 'A'; ?>
+
+                    <?php for ($i = 0; $i < sizeof($data) - 1; $i++) { ?>
+                        <div class="ui celled list">
+                            <div class="item">
+                                <div class="shadow p-3">
+
+                                    <a href="<?php echo URLROOT; ?>problems/delete/<?php echo $data[$i]->id; ?>"><i class="trash alternate icon text-danger"></i></a>
+
+                                    <a target="_blank" href="<?php echo URLROOT; ?>problems/show/<?php echo $data[$i]->id; ?>"><?php echo $x++; ?></a>
+                                    -
+                                    <a target="_blank" href="<?php echo URLROOT; ?>problems/show/<?php echo $data[$i]->id; ?>"><?php echo $data[$i]->name; ?></a>
+                                </div>
+                            </div>
+                        </div>
+                    <?php } ?>
+                </div>
+            <?php } ?>
         </div>
+
+
 
         <!-- Right Side Bar -->
         <?php include APPROOT . '/views/inc/rightSideBar.php'; ?>
